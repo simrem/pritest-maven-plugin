c01a6df88e2a1ae8253b2ca6b6666f6a4e3ae818
Sveinung Dalatun <sdalatun@gmail.com>
2011-03-31 12:58:08 +0200

    CompilationUnitVisitor now supports import declarations

--- src/main/java/no/citrus/localprioritization/CompilationUnitVisitor.java
+++ src/main/java/no/citrus/localprioritization/CompilationUnitVisitor.java
@@ -1,25 +1,85 @@
 package no.citrus.localprioritization;
 
-import japa.parser.ast.CompilationUnit;
-import japa.parser.ast.body.TypeDeclaration;
-import japa.parser.ast.visitor.GenericVisitorAdapter;
-
 import java.util.ArrayList;
 import java.util.List;
 
-public class CompilationUnitVisitor extends GenericVisitorAdapter<List<ClassType>, Object>{
+import japa.parser.ast.CompilationUnit;
+import japa.parser.ast.ImportDeclaration;
+import japa.parser.ast.body.TypeDeclaration;
+import japa.parser.ast.expr.NameExpr;
+import japa.parser.ast.expr.QualifiedNameExpr;
+import japa.parser.ast.visitor.VoidVisitorAdapter;
 
+public class CompilationUnitVisitor extends VoidVisitorAdapter<Object> {
+
+	private List<ClassType> types;
+	private List<String> importStatements;
+	
+	public CompilationUnitVisitor() {
+		this.types = new ArrayList<ClassType>();
+		this.importStatements = new ArrayList<String>();
+	}
+	
 	@Override
-	public List<ClassType> visit(CompilationUnit cu, Object obj) {
-		ArrayList<ClassType> classes = new ArrayList<ClassType>();
-		
+	public void visit(CompilationUnit cu, Object obj) {
 		if (cu.getTypes() != null) {
 			for (TypeDeclaration td : cu.getTypes()) {
-				//ClassType ct = td.accept(new ClassOrInterfaceDeclarationVisitor(), obj);
-				//classes.add(ct);
+				ClassOrInterfaceDeclarationVisitor cidv = new ClassOrInterfaceDeclarationVisitor();
+				td.accept(cidv, null);
+				this.types = cidv.getTypes();
 			}
 		}
 		
-		return classes;
+		if (cu.getImports() != null) {
+			for (ImportDeclaration id : cu.getImports()) {
+				ImportVisitor iv = new ImportVisitor();
+				id.accept(iv, null);
+				this.importStatements.add(iv.getImportStatement());
+			}
+		}
+	}
+
+	public List<ClassType> getTypes() {
+		return types;
+	}
+
+	public List<String> getImportStatements() {
+		return this.importStatements;
+	}
+	
+	private class ImportVisitor extends VoidVisitorAdapter<String> {
+
+		private String importStatement;
+		
+		public ImportVisitor() {
+			this.importStatement = null;
+		}
+
+		@Override
+		public void visit(ImportDeclaration n, String arg) {
+			n.getName().accept(this, arg);
+		}
+
+		@Override
+		public void visit(NameExpr n, String arg) {
+			this.importStatement = n.getName() + "." + this.importStatement;
+		}
+
+		@Override
+		public void visit(QualifiedNameExpr n, String arg) {
+			if (this.importStatement == null) {
+				this.importStatement = n.getName();
+			} else {
+				this.importStatement = n.getName() + "." + this.importStatement;
+			}
+			
+			if (n.getQualifier() != null) {
+				n.getQualifier().accept(this, arg);
+			}
+		}
+
+		public String getImportStatement() {
+			return importStatement;
+		}
 	}
 }
