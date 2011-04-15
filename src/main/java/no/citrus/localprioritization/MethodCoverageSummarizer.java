e79d77f5b70e95ddfc889572bb48892d80a2ce20
Sveinung Dalatun <sdalatun@gmail.com>
2011-04-15 12:24:52 +0200

    supports transitive method calls

--- src/main/java/no/citrus/localprioritization/MethodCoverageSummarizer.java
+++ src/main/java/no/citrus/localprioritization/MethodCoverageSummarizer.java
@@ -1,27 +1,72 @@
 package no.citrus.localprioritization;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import no.citrus.localprioritization.model.ClassCover;
 import no.citrus.localprioritization.model.MethodCover;
+import no.citrus.localprioritization.model.ProcessedMethodCall;
 
 public class MethodCoverageSummarizer {
 
 	private final Map<String, ClassCover> coveredClasses;
+	private Map<String, MethodCover> transitiveMethodCalls;
 
-	public MethodCoverageSummarizer(Map<String, ClassCover> coveredClasses) {
+	public MethodCoverageSummarizer(Map<String, ClassCover> coveredClasses, ClassCover coveredTestCase) {
 		this.coveredClasses = coveredClasses;
+		
+		transitiveMethodCalls = summarizeCoverageOfTestCase(coveredTestCase);
 	}
 
-	public ClassCover summarizeCoverageOfTestCase(ClassCover coveredTestCase) {
+	private Map<String, MethodCover> summarizeCoverageOfTestCase(ClassCover coveredTestCase) {
 		Map<String, MethodCover> methods = coveredTestCase.getMethods();
 		
-		Map<String, MethodCover> transitiveMethods = new HashMap<String, MethodCover>();
+		Map<String, MethodCover> transitiveMethodCalls = new HashMap<String, MethodCover>();
 		
-		//for ()
+		for (MethodCover methodCover : methods.values()) {
+			List<ProcessedMethodCall> methodCalls = methodCover.getMethodCalls();
+			for (ProcessedMethodCall processedCall : methodCalls) {
+				ClassCover calledClass = coveredClasses.get(processedCall.getClassName());
+				if (calledClass != null) {
+					MethodCover calledMethod = calledClass.getMethods().get(processedCall.getMethodName());
+					if (calledMethod != null) {
+						transitiveMethodCalls = summarizeRecursively(calledMethod, transitiveMethodCalls);
+					}
+				}
+			}
+		}
 		
-		return coveredTestCase;
+		return transitiveMethodCalls;
 	}
 
+	private Map<String, MethodCover> summarizeRecursively(MethodCover methodCall,
+			Map<String, MethodCover> transitiveMethodCalls) {
+		
+		if (transitiveMethodCalls.get(makeClassAndMethodNameString(methodCall)) == null) {
+			
+			transitiveMethodCalls.put(makeClassAndMethodNameString(methodCall), methodCall);
+			
+			List<ProcessedMethodCall> methodCalls = methodCall.getMethodCalls();
+			for (ProcessedMethodCall processedCall : methodCalls) {
+				ClassCover calledClass = coveredClasses.get(processedCall.getClassName());
+				if (calledClass != null) {
+					MethodCover calledMethod = calledClass.getMethods().get(processedCall.getMethodName());
+					if (calledMethod != null) {
+						transitiveMethodCalls = summarizeRecursively(calledMethod, transitiveMethodCalls);
+					}
+				}
+			}
+		}
+		
+		return transitiveMethodCalls;
+	}
+
+	private String makeClassAndMethodNameString(MethodCover methodCall) {
+		return methodCall.getClassName() + "." + methodCall.getMethodName();
+	}
+
+	public Map<String, MethodCover> getSummarizedCoverage() {
+		return transitiveMethodCalls;
+	}
 }
--- src/test/java/no/citrus/localprioritization/MethodCoverageSummarizerTest.java
+++ src/test/java/no/citrus/localprioritization/MethodCoverageSummarizerTest.java
@@ -10,41 +10,70 @@ import no.citrus.localprioritization.model.MethodCover;
 import no.citrus.localprioritization.model.ProcessedMethodCall;
 import no.citrus.localprioritization.model.ReferenceType;
 
-import org.junit.Ignore;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.collection.IsCollectionContaining.hasItems;
+import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.assertThat;
 
 public class MethodCoverageSummarizerTest {
 	
-	@Test
-	@Ignore
-	public void should_find_transitive_method_calls() {
+	private Map<String, MethodCover> summarizedCoverageOfTestCase;
+
+	@Before
+	public void setup() {
 		Map<String, ClassCover> coveredClasses = new HashMap<String, ClassCover>();
 		
 		ClassCover coveredClassA = new ClassCover("A");
 		List<ProcessedMethodCall> methodCallsA = new ArrayList<ProcessedMethodCall>();
 		methodCallsA.add(new ProcessedMethodCall("B", "b", new ArrayList<String>()));
-		coveredClassA.getMethods().put("a", new MethodCover("A", "void", "a", new ArrayList<ReferenceType>(), methodCallsA));
+		methodCallsA.add(new ProcessedMethodCall("C", "c", new ArrayList<String>()));
+		coveredClassA.getMethods().put("a", new MethodCover("A", "void", "a",
+				new ArrayList<ReferenceType>(), methodCallsA));
 		coveredClasses.put("A", coveredClassA);
 		
 		ClassCover coveredClassB = new ClassCover("B");
+		List<ProcessedMethodCall> methodCallsB = new ArrayList<ProcessedMethodCall>();
+		//methodCallsB.add(new ProcessedMethodCall("C", "c", new ArrayList<String>()));
 		coveredClassB.getMethods().put("b", new MethodCover("B", "void", "b", 
-				new ArrayList<ReferenceType>(), new ArrayList<ProcessedMethodCall>()));
+				new ArrayList<ReferenceType>(), methodCallsB));
 		coveredClasses.put("B", coveredClassB);
 		
+		ClassCover coveredClassC = new ClassCover("C");
+		coveredClassB.getMethods().put("c", new MethodCover("C", "void", "c", 
+				new ArrayList<ReferenceType>(), new ArrayList<ProcessedMethodCall>()));
+		coveredClasses.put("C", coveredClassC);
+		
 		ClassCover coveredTestCase = new ClassCover("ATest");
+		List<ProcessedMethodCall> methodsCoveredByTest = new ArrayList<ProcessedMethodCall>();
+		methodsCoveredByTest.add(new ProcessedMethodCall("A", "a", new ArrayList<String>()));
+		coveredTestCase.getMethods().put("should_bla_bla", new MethodCover("ATest", "void", "should_bla_bla", 
+				new ArrayList<ReferenceType>(), methodsCoveredByTest));
 		
-		MethodCoverageSummarizer mcs = new MethodCoverageSummarizer(coveredClasses);
-		ClassCover summarizedCoverageOfTestCase = mcs.summarizeCoverageOfTestCase(coveredTestCase);
+		MethodCoverageSummarizer mcs = new MethodCoverageSummarizer(coveredClasses, coveredTestCase);
+		summarizedCoverageOfTestCase = mcs.getSummarizedCoverage();
+	}
+	
+	@Test
+	public void should_find_direct_method_calls() {
+		List<ProcessedMethodCall> answerMethodCallsA = new ArrayList<ProcessedMethodCall>();
+		answerMethodCallsA.add(new ProcessedMethodCall("B", "b", new ArrayList<String>()));
+		answerMethodCallsA.add(new ProcessedMethodCall("C", "c", new ArrayList<String>()));
 		
-		List<ProcessedMethodCall> answerMethodCalls = new ArrayList<ProcessedMethodCall>();
-		answerMethodCalls.add(new ProcessedMethodCall("B", "b", new ArrayList<String>()));
-		
-		assertThat(summarizedCoverageOfTestCase.getMethods().values(), hasItems(
-				new MethodCover("A", "void", "a", new ArrayList<ReferenceType>(), answerMethodCalls),
+		assertThat(summarizedCoverageOfTestCase.values(), hasItems(
+				new MethodCover("A", "void", "a", new ArrayList<ReferenceType>(), answerMethodCallsA),
 				new MethodCover("B", "void", "b", new ArrayList<ReferenceType>(), new ArrayList<ProcessedMethodCall>())
 		));
+	}
+	
+	@Test
+	public void should_not_include_test_methods() {
+		List<ProcessedMethodCall> testCaseMethodCalls = new ArrayList<ProcessedMethodCall>();
+		testCaseMethodCalls.add(new ProcessedMethodCall("A", "a", new ArrayList<String>()));
+		
+		assertThat(summarizedCoverageOfTestCase.values(), not(hasItems(
+				new MethodCover("ATest", "void", "should_bla_bla", new ArrayList<ReferenceType>(), testCaseMethodCalls)
+		)));
 	}
 }
