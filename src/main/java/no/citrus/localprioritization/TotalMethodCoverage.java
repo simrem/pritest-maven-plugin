b8c6ecdb5310b126e0b93b630c897ba093b4ad75
Sveinung Dalatun <sdalatun@gmail.com>
2011-04-25 20:43:03 +0200

    TotalMethodCoverage mostly implemented

--- src/main/java/no/citrus/localprioritization/TotalMethodCoverage.java
+++ src/main/java/no/citrus/localprioritization/TotalMethodCoverage.java
@@ -2,30 +2,48 @@ package no.citrus.localprioritization;
 
 import japa.parser.ParseException;
 import japa.parser.ast.CompilationUnit;
+import no.citrus.localprioritization.model.ClassCover;
+import no.citrus.localprioritization.model.ClassType;
+import no.citrus.localprioritization.model.MethodCover;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-
-import no.citrus.localprioritization.model.ClassCover;
-import no.citrus.localprioritization.model.ClassType;
+import java.util.*;
 
 public class TotalMethodCoverage {
 	private List<ClassCover> testCases;
 
 	public TotalMethodCoverage(String pathToProjectSource, String pathToTestSuite)
 			throws ParseException, IOException {
-		
-		Map<String, ClassCover> sourceClassCover = retrieveClassCoverage(pathToProjectSource);
-		Map<String, ClassCover> testClassCover = retrieveClassCoverage(pathToTestSuite);
-		
-		Collection<ClassCover> testCaseCollection = testClassCover.values();
-		
+
+        testCases = new ArrayList<ClassCover>();
+
+        prioritizeTestCases(pathToProjectSource, pathToTestSuite);
 	}
 
-	private Map<String, ClassCover> retrieveClassCoverage(String pathToProjectSource)
+    private void prioritizeTestCases(String pathToProjectSource, String pathToTestSuite) throws ParseException, IOException {
+        Map<String, ClassCover> sourceClassCover = retrieveClassCoverage(pathToProjectSource);
+        Map<String, ClassCover> testClassCover = retrieveClassCoverage(pathToTestSuite);
+
+        List<SummarizedTestCase> prioritizedTestCases = new ArrayList<SummarizedTestCase>();
+
+        Collection<ClassCover> testCaseCollection = testClassCover.values();
+        for (ClassCover testCase : testCaseCollection) {
+            MethodCoverageSummarizer mcs = new MethodCoverageSummarizer(sourceClassCover, testCase);
+            Map<String, MethodCover> summarizedCoverage = mcs.getSummarizedCoverage();
+            SummarizedTestCase summarizedTestCase = new SummarizedTestCase(testCase, summarizedCoverage);
+            prioritizedTestCases.add(summarizedTestCase);
+        }
+
+        Collections.sort(prioritizedTestCases);
+
+        for (SummarizedTestCase stc : prioritizedTestCases) {
+            testCases.add(stc.getTestCase());
+            System.out.println(stc.getTestCase().getName() + " " + stc.getSummarizedCoverage().size());
+        }
+    }
+
+    private Map<String, ClassCover> retrieveClassCoverage(String pathToProjectSource)
 			throws ParseException, IOException {
 		
 		List<File> fileList = 
@@ -44,4 +62,29 @@ public class TotalMethodCoverage {
 	public List<ClassCover> getTestCases() {
 		return testCases;
 	}
+
+    private class SummarizedTestCase implements Comparable<SummarizedTestCase> {
+
+        private ClassCover testCase;
+        private Map<String, MethodCover> summarizedCoverage;
+
+        public SummarizedTestCase(ClassCover testCase, Map<String, MethodCover> summarizedCoverage) {
+
+            this.testCase = testCase;
+            this.summarizedCoverage = summarizedCoverage;
+        }
+
+        public ClassCover getTestCase() {
+            return testCase;
+        }
+
+        public Map<String, MethodCover> getSummarizedCoverage() {
+            return summarizedCoverage;
+        }
+
+        public int compareTo(SummarizedTestCase summarizedTestCase) {
+            Integer sizeOfThis = new Integer(summarizedCoverage.size());
+            return sizeOfThis.compareTo(new Integer(summarizedTestCase.getSummarizedCoverage().size()));
+        }
+    }
 }
--- src/test/java/no/citrus/localprioritization/TotalMethodCoverageTest.java
+++ src/test/java/no/citrus/localprioritization/TotalMethodCoverageTest.java
@@ -1,14 +1,11 @@
 package no.citrus.localprioritization;
 
 import japa.parser.ParseException;
+import no.citrus.localprioritization.model.ClassCover;
+import org.junit.Test;
 
 import java.io.IOException;
 import java.util.List;
-
-import no.citrus.localprioritization.model.ClassCover;
-
-import org.junit.Ignore;
-import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
@@ -17,7 +14,6 @@ import static org.junit.Assert.assertThat;
 public class TotalMethodCoverageTest {
 	
 	@Test
-	@Ignore
 	public void should_give_a_list_of_test_cases_with_size_greater_than_zero() throws ParseException, IOException {
 		TotalMethodCoverage tmc = new TotalMethodCoverage("src/main/java", "src/test/java");
 		List<ClassCover> testCases = tmc.getTestCases();
