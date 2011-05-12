fdcea54d6eabf3566b86ddaa2a2943d51366527e
simrem <simon.remoy@gmail.com>
2011-05-12 13:19:56 +0200

    printing number of method calls

--- src/main/java/no/citrus/localprioritization/TotalMethodCoverage.java
+++ src/main/java/no/citrus/localprioritization/TotalMethodCoverage.java
@@ -25,7 +25,7 @@ public class TotalMethodCoverage extends MethodCoverage {
 
         for (SummarizedTestCase stc : prioritizedTestCases) {
             addTestCase(stc);
-//            System.out.println(stc.getTestCase().getPackageName() + "." + stc.getTestCase().getName() + " " + stc.getSummarizedCoverage().size());
+            System.out.println(stc.getTestCase().getPackageName() + "." + stc.getTestCase().getName() + " " + stc.getSummarizedCoverage().size());
         }
     }
 }
